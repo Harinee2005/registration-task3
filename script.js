549aaa9eb8bff2039ce818d88807116613ba97c5
Harinee S <152970769+Harinee2005@users.noreply.github.com>
2024-08-21 21:08:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function validateForm() {
+    if (document.getElementById('firstName').value === "") {
+        alert("First Name is required..");
+        return 0;
+    }
+
+    if (document.getElementById('lastName').value === "") {
+        alert("Last Name is required..");
+        return 0;
+    }
+
+    if (document.getElementById('email').value === "") {
+        alert("Email is required..");
+        return 0;
+    }
+
+    if (document.getElementById('email').value.indexOf('@') === -1 || document.getElementById('email').value.indexOf('.') === -1) {
+        alert("Invalid email format..");
+        return 0;
+    }
+
+    if (document.getElementById('contact').value === "") {
+        alert("Contact Number is required..");
+        return 0;
+    }
+
+    if (isNaN(document.getElementById('contact').value)) {
+        alert("Contact Number must be in numeric..");
+        return 0;
+    }
+
+    if ((document.getElementById('contact').value.length !== 10)) {
+        alert("Invalid Mobile Number..");
+        return 0;
+    }
+
+    if (document.getElementById('address').value === "") {
+        alert("Address is required.");
+        return 0;
+    }
+
+    if (document.getElementById('city').value === "") {
+        alert("City is required.");
+        return 0;
+    }
+
+    else{
+        alert("Registered Successfully...");
+    }
+}
